78a54023eaa8169cbfcc074ae9340857ae742ece
Manjunath Irukulla <imanjunad@gmail.com>
2024-10-18 19:31:03 +0530

    main function

---
+++ README.md
@@ -0,0 +1,1 @@
+# MultiThreaded Proxy Web Server
---
+++ proxy_server.c
@@ -0,0 +1,25 @@
+#include "proxy_parse.h";
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <pthread.h>
+#define MAX_CLIENTS 10
+typedef struct cache_element cache_element; 
+
+struct cache_element_list{
+    char* data;
+    int len;
+    char* url;
+    time_t lru_time_track;
+    cache_element* next;
+};
+
+cache_element* find(char* url);
+int add_cache_element(char* data, int size, char url);
+void remove_cache_element();
+int port_number = 3018;
+int proxy_sockerId;
+pthread_t tid(MAX_CLIENTS);
+sem_t semaphore;
+pthread_mutex_t lock;
